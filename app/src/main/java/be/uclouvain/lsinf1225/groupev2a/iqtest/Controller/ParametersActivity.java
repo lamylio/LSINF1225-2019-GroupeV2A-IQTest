1db7e24146d5771dee24ed5cfa7febcd19c3e288
RomainP <romain.petit@student.uclouvain.be>
2019-05-01 12:22:45 +0200

    Ajout de l'activité Parametres et lien entre le bouton paramètres et la nouvelle activité, reste à savoir quoi mettre sur cette nouvelle page

--- app/src/main/java/be/uclouvain/lsinf1225/groupev2a/iqtest/Controller/ParametersActivity.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupev2a/iqtest/Controller/ParametersActivity.java
@@ -1,13 +1,10 @@
 package be.uclouvain.lsinf1225.groupev2a.iqtest.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
-import be.uclouvain.lsinf1225.groupev2a.iqtest.Database.Table.User;
-import be.uclouvain.lsinf1225.groupev2a.iqtest.R;
-import be.uclouvain.lsinf1225.groupev2a.iqtest.Utils;
-import android.view.View;
-
 
 import android.os.Bundle;
+import be.uclouvain.lsinf1225.groupev2a.iqtest.R;
+import be.uclouvain.lsinf1225.groupev2a.iqtest.Utils;
 
 public class ParametersActivity extends AppCompatActivity {
 
