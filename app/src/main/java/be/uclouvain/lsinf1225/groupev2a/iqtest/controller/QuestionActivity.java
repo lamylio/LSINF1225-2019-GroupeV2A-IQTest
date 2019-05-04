4b9224ac923564e89f6a338db69934d26a8597b2
victorfischer <victorfischer0808@gmail.com>
2019-05-04 15:14:53 +0200

    Début de QuestionActivity, à faire :  - Timer à faire  - Relier les boutons  - Ajouter l'endroit où il y a la question  - Relier à la bdd

--- app/src/main/java/be/uclouvain/lsinf1225/groupev2a/iqtest/controller/QuestionActivity.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupev2a/iqtest/controller/QuestionActivity.java
@@ -1,34 +1,30 @@
 package be.uclouvain.lsinf1225.groupev2a.iqtest.controller;
 
-        import android.os.Bundle;
-        import android.os.CountDownTimer;
-        import android.widget.Button;
+import android.os.Bundle;
+import android.widget.Button;
 
-        import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
-        import be.uclouvain.lsinf1225.groupev2a.iqtest.R;
+import be.uclouvain.lsinf1225.groupev2a.iqtest.R;
 
 public class QuestionActivity extends AppCompatActivity {
     Button choice1;
     Button choice2;
     Button choice3;
     Button choice4;
-    CountDownTimer timer = new CountDownTimer() {
-        @Override
-        public void onTick(long millisUntilFinished) {
-
-        }
-
-        @Override
-        public void onFinish() {
-
-        }
-    }
+    Button suivant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        suivant = findViewById(R.id.pg_suivante);
+        choice1=findViewById(R.id.rep1);
+        choice2=findViewById(R.id.rep2);
+        choice3=findViewById(R.id.rep3);
+        choice4=findViewById(R.id.rep4);
+
     }
 
 
