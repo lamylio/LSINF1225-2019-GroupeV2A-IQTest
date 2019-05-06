2ee4bf60956d2c73bba5dcca345e94a4eeb1b51d
lamylio <lionel.lamy@student.uclouvain>
2019-05-06 16:17:36 +0200

    Merge branch 'master' of /Users/lioche/AndroidStudioProjects/LSINF1225-2019-GroupeV2A-IQTest with conflicts.

---
+++ app/src/main/java/be/uclouvain/lsinf1225/groupev2a/iqtest/controller/game/GameActivity.java
@@ -0,0 +1,16 @@
+package be.uclouvain.lsinf1225.groupev2a.iqtest.controller.game;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import be.uclouvain.lsinf1225.groupev2a.iqtest.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_game);
+    }
+}
